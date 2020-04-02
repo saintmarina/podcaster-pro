2eaabff28c888ca826b7c79d55ff718d22ed6027
Anna Maniuk <anna@maniuk.nyc>
2020-04-02 00:21:24 -0400

    VolumeVisualizer WIP (bars, colors  -> DONE)

--- app/src/main/java/com/example/recordingsystem/SoundVisualizer.kt
+++ app/src/main/java/com/example/recordingsystem/SoundVisualizer.kt
@@ -2,14 +2,16 @@ package com.example.recordingsystem
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
 class SoundVisualizer (context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
-
-    val painter = Paint()
+    private val painterGreen = Paint().apply { color = Color.parseColor("#32CD32") }
+    private val painterYellow = Paint().apply { color = Color.parseColor("#FFFF00") }
+    private val painterRed = Paint().apply { color = Color.parseColor("#FF0000") }
 
     var volume: Short = 0
         set(value) {
@@ -17,24 +19,26 @@ class SoundVisualizer (context: Context, attributeSet: AttributeSet) : View(cont
             invalidate()
         }
 
-
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val parentWidth = MeasureSpec.getSize(widthMeasureSpec)
-        val desiredHeight = 200
-
-        setMeasuredDimension(parentWidth, desiredHeight)
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
-
-
-
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
         canvas?.let {
-            val right = (width*volume)/Short.MAX_VALUE
-            val rect = Rect(0, 0, right, height)
-            it.drawRect(rect, painter)
+            val max = (width*volume)/Short.MAX_VALUE
+            val spacer = 3
+            val rectWidth = 7
+            val fullRectWidth = spacer + rectWidth
+
+            for (px in 0..max step fullRectWidth) {
+                val rect = Rect(px, 0, px+rectWidth, height)
+
+                val paintColor = when {
+                    px > width*0.75 -> painterRed
+                    px > width*0.5 -> painterYellow
+                    else -> painterGreen
+                }
+
+                it.drawRect(rect, paintColor)
+            }
         }
 
     }
