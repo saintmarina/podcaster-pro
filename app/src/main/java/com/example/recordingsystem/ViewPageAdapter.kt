7d17e05c14f619c22cbf1fc570e28766381d1abf
Anna Maniuk <anna@maniuk.nyc>
2020-04-17 13:32:13 -0400

    Refactoring done. Custom notification layouts DELETED

--- app/src/main/java/com/example/recordingsystem/ViewPageAdapter.kt
+++ app/src/main/java/com/example/recordingsystem/ViewPageAdapter.kt
@@ -4,16 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_page.view.*
+import kotlinx.android.synthetic.main.view_pager_items_page.view.*
 
 class ViewPagerAdapter : RecyclerView.Adapter<PagerVH>() {
-
-    //array of colors to change the background color of screen
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerVH =
-        PagerVH(LayoutInflater.from(parent.context).inflate(R.layout.item_page, parent, false))
+        PagerVH(LayoutInflater.from(parent.context).inflate(R.layout.view_pager_items_page, parent, false))
 
-    //get the size of color array
     override fun getItemCount(): Int = 3
 
     //binding the screen with view
