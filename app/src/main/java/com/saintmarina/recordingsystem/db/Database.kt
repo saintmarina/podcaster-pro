effd162e6dc78a335b719ad2746f1d8f3ff8edb8
Anna Maniuk <anna@maniuk.nyc>
2020-06-23 13:41:50 -0400

    Database file clean up

--- app/src/main/java/com/saintmarina/recordingsystem/db/Database.kt
+++ app/src/main/java/com/saintmarina/recordingsystem/db/Database.kt
@@ -3,13 +3,6 @@ package com.saintmarina.recordingsystem.db
 import android.content.Context
 import androidx.room.*
 
-
-// TODO replicate the FileMetadata object
-// TODO Replace this with a https://developer.android.com/training/data-storage/room entity+Dao
-// There should be a new folder in recordingsystem: db (like service or ui)
-// In this folder, there should be a Database.kt and a FileMetadata.kt file // TODO Do this first
-// all folders should be lowercase, so UI -> ui, googleDrive -> googledrive
-
 @androidx.room.Database(entities = [FileMetadata::class], version = 1)
 abstract class Database: RoomDatabase() {
     abstract fun fileMetadataDao(): FileMetadataDao
