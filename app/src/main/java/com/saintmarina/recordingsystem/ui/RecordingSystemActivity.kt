d74f1700c37cb0aac5080669bd8a8bf78bdf5636
Anna Maniuk <anna@maniuk.nyc>
2020-06-30 21:04:18 -0400

    wip

--- app/src/main/java/com/saintmarina/recordingsystem/ui/RecordingSystemActivity.kt
+++ app/src/main/java/com/saintmarina/recordingsystem/ui/RecordingSystemActivity.kt
@@ -51,15 +51,11 @@ Status:
  * NICE TO HAVE:
  * Sound notification when recording time reached 2:45 hrs
  * Add max sound bar for the past two seconds
- * Card view instead of viewPager2
  * Make sure that the sound recorded on software on the device is the same as recorded on tablet
- * Sound notification when recording time reached 2:45 hrs
  * Add max sound bar for the past two seconds
- * Card view instead of viewPager2
  * Recovery files should have their header correctly set when uploaded
  * The audio feedback bipbip should seems delayed. It should not.
  *
- * Tell the user clipping occurred when stopping a recording if it occurred
  */
 // MDM make sure it doesn't have the title bar
 // TODO
