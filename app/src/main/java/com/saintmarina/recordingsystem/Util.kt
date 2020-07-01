123846ce094569bc949f19b5939380e178dc33cf
Anna Maniuk <anna@maniuk.nyc>
2020-07-01 11:45:16 -0400

    Long filename bug fix
    Clean up

--- app/src/main/java/com/saintmarina/recordingsystem/Util.kt
+++ app/src/main/java/com/saintmarina/recordingsystem/Util.kt
@@ -26,16 +26,7 @@ object Util {
 
     @JvmStatic fun prettyDuration(time: Int): String {
         val seconds = time % 60
-        val minutes = time / 60 % 60
+        val minutes = time / 60
         return if (minutes > 0) "${minutes} min" else "${seconds} sec"
     }
-
-    // Might be useful
-    /*
-    private fun readFileContent(file: File): String {
-        val stream = FileInputStream(file)
-        val byteBuffer = stream.channel.map(FileChannel.MapMode.READ_ONLY, 0, stream.channel.size())
-        return Charset.defaultCharset().decode(byteBuffer).toString()
-    }
-     */
 }
